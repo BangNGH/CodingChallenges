027c6d8dcbfe156b8829f2bb6dac1ac3943b02c8
nguyenvanvinh1012 <nguyenvanvinhisco22@gmail.com>
2023-10-04 08:51:47 +0700

    first commit

---
+++ src/main/java/com/example/coderlab/CoderLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coderlab/CoderLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
